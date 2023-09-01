bbf631b1c9908390ade5beb0c5542c9fca6f97fd
Troshchk <ktroshchenkova@ripe.net>
2023-09-01 22:00:30 +0200

    Add template for main page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def welcome():
+    return render_template("main.html")
+
+
 @app.get("/sites")
 def sites_get():
     return "Sites"
