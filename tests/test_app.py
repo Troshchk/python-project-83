90b418f812f4de633dc188ac58dfae7b87c83a74
Troshchk <ktroshchenkova@ripe.net>
2023-09-01 20:03:17 +0200

    Add tests

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+import page_analyzer.app as app
+import pytest
+
+
+def test_initial():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.text == "Welcome!"
