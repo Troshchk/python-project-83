5bf528112b7db49e9fee5b1f04963a39d98f8ab5
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-13 17:12:39 +0200

    Add setup to connect to the DB

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-build:
+build-p:
 	poetry build
 
 publish:
@@ -22,3 +22,6 @@ dev:
 PORT ?= 8000
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
+
+build:
+	./build.sh
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 
 @app.route("/")
