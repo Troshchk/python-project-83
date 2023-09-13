f275328167c2cbd8af923db7643e60a7201c39fd
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-13 17:13:21 +0200

    Add and validate input urls

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,6 +5,33 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
+@app.post("/urls")
+def add_url():
+    url_to_add = request.form.to_dict()["url"]
+    if validators.url(url_to_add) and len(url_to_add)<256:
+        try:
+            conn = psycopg2.connect(DATABASE_URL)
+        except:
+            print('Can`t establish connection to database')
+        with conn.cursor() as curs:
+            curs.execute('SELECT id FROM urls WHERE name=%s', (url_to_add,))
+            id = curs.fetchall()
+        if len(id)>0:
+            flash('Страница уже существует', 'info')
+            return redirect(url_for('show_url_by_id', id=id[0][0]))
+        with conn.cursor() as curs:
+            curs.execute('INSERT INTO urls(name, created_at) VALUES (%s, %s)', (url_to_add, date.today().isoformat(),))
+            curs.execute('SELECT * FROM urls WHERE name=%s', (url_to_add,))
+            conn.commit()
+            curs.execute('SELECT * FROM urls WHERE name=%s', (url_to_add,))
+            id = curs.fetchall()
+        conn.close()
+        flash('Страница успешно добавлена', 'success')
+        return redirect(url_for('show_url_by_id', id=id[0][0]))
+    flash('Некорректный URL', 'danger')
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('main.html', messages=messages), 422
+
 
 @app.route("/")
 def welcome():
