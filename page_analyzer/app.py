bc28a2d428bdab67e718e544dbe9780f0ea20721
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-13 17:31:22 +0200

    Fix linter errors

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, get_flashed_messages
+from flask import Flask, render_template, request, flash, redirect, \
+    url_for, get_flashed_messages
 import validators
 from datetime import date
 import psycopg2
@@ -11,15 +12,16 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
+
 @app.get("/")
 def main_page():
-    return render_template("main.html", errors = None)
+    return render_template("main.html", errors=None)
 
 
 @app.post("/urls")
 def add_url():
     url_to_add = request.form.to_dict()["url"]
-    if validators.url(url_to_add) and len(url_to_add)<256:
+    if validators.url(url_to_add) and len(url_to_add) < 256:
         try:
             conn = psycopg2.connect(DATABASE_URL)
         except:
@@ -27,11 +29,13 @@ def add_url():
         with conn.cursor() as curs:
             curs.execute('SELECT id FROM urls WHERE name=%s', (url_to_add,))
             id = curs.fetchall()
-        if len(id)>0:
+        if len(id) > 0:
             flash('Страница уже существует', 'info')
             return redirect(url_for('show_url_by_id', id=id[0][0]))
         with conn.cursor() as curs:
-            curs.execute('INSERT INTO urls(name, created_at) VALUES (%s, %s)', (url_to_add, date.today().isoformat(),))
+            curs.execute('INSERT INTO urls(name, created_at) \
+                         VALUES (%s, %s)',
+                         (url_to_add, date.today().isoformat(),))
             curs.execute('SELECT * FROM urls WHERE name=%s', (url_to_add,))
             conn.commit()
             curs.execute('SELECT * FROM urls WHERE name=%s', (url_to_add,))
@@ -57,7 +61,6 @@ def show_urls():
     return render_template('urls_all.html', urls=urls)
 
 
-
 @app.get("/urls/<id>")
 def show_url_by_id(id):
     try:
@@ -65,12 +68,13 @@ def show_url_by_id(id):
     except:
         print('Can`t establish connection to database')
     with conn.cursor() as curs:
-            curs.execute('SELECT * FROM urls WHERE id=%s', (id,))
-            url_to_show = curs.fetchall()
+        curs.execute('SELECT * FROM urls WHERE id=%s', (id,))
+        url_to_show = curs.fetchall()
     conn.close()
-    if len(url_to_show)<1:
+    if len(url_to_show) < 1:
         return "NOT FOUND"
     name = url_to_show[0][1]
     created_at = url_to_show[0][2]
     messages = get_flashed_messages(with_categories=True)
-    return render_template('individual_url.html', messages=messages, name=name, created_at=created_at, id=id), 200
+    return render_template('individual_url.html', messages=messages,
+                           name=name, created_at=created_at, id=id), 200
