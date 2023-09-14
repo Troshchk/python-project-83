0f5f1298535605524821e833b79f2ceb0ca5d21a
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-14 17:04:35 +0200

    Allow only induvudual websites, excluding pages and other parameters

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -8,6 +8,7 @@ import os
 from dotenv import load_dotenv
 from collections import namedtuple
 import requests
+from urllib.parse import urlparse, urlunparse
 
 load_dotenv()
 app = Flask(__name__)
@@ -23,6 +24,8 @@ def main_page():
 @app.post("/urls")
 def add_url():
     url_to_add = request.form.to_dict()["url"]
+    url_to_add = urlparse(url_to_add)._replace(path="")._replace(params="")._replace(query="")._replace(fragment="").geturl()
+    print(url_to_add)
     if validators.url(url_to_add) and len(url_to_add) < 256:
         try:
             conn = psycopg2.connect(DATABASE_URL)
