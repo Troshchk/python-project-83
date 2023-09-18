88b479707f7af0b2c85c7517ee4d1ea64b7e5665
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-18 20:18:29 +0200

    Add tests and test db

---
+++ test.sql
@@ -0,0 +1,18 @@
+CREATE TABLE urls(
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255),
+    created_at date);
+
+
+CREATE TABLE url_checks(
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls (id),
+    status_code int,
+    h1 varchar(255),
+    title varchar(255),
+    description text,
+    created_at date);
+
+
+INSERT INTO urls(name, created_at) VALUES ('https://www.google.com', '2023-09-14');
+INSERT INTO urls(name, created_at) VALUES ('http://docs.python.org:80', '2023-09-14');
---
+++ tests/test_url.py
@@ -0,0 +1,13 @@
+import pytest
+from page_analyzer.url import URL
+import validators
+
+
+@pytest.mark.parametrize(
+    "url, expected",
+    [('https://www.google.com', False),
+    ('not_valid', True),
+    ('https://www.goooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogle.com', True)])
+def test_url_formatting(url, expected):
+    print(type(URL(url).is_valid()))
+    assert isinstance(URL(url).is_valid(), validators.utils.ValidationError) == expected
