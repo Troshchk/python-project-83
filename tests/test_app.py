0491c01a1d3c01bba84330bca2b40c3c1eef4f88
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-19 11:20:32 +0200

    Remove db setup -- not needed

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,31 +1,9 @@
 import page_analyzer.app as app
 import pytest
 import os
-from pytest_postgresql.janitor import DatabaseJanitor
-import psycopg2
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-
-@pytest.fixture
-def database(postgresql_proc):
-    janitor = DatabaseJanitor(
-        postgresql_proc.user,
-        postgresql_proc.host,
-        postgresql_proc.port,
-        "my_test_database",
-        postgresql_proc.version,
-        password="secret_password",
-    )
-    janitor.init()
-    db = psycopg2.connect(DATABASE_URL)
-    with open("test.sql") as f:
-        setup_sql = f.read()
-    with db.cursor() as cursor:
-        cursor.execute(setup_sql)
-        db.commit()
-    yield db
-    janitor.drop()
 
 
 def test_initial():
