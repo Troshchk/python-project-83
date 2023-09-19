f729442928ce7b5e87fed54e7145045abf356a8c
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-19 14:21:26 +0200

    Add print for logging

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -24,6 +24,8 @@ page_analyser = Page_analyzer(db_manager=db_manager)
 
 @app.get("/")
 def main_page():
+    print(db_manager.connection)
+    print(os.getenv("DATABASE_URL"))
     return render_template("main.html", errors=None)
 
 
