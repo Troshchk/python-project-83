0eb0cab65becea500adf30d8fcd39a1139d01108
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-23 19:05:22 +0200

    Move setup and teardown of db from workflow to tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,28 @@
 import page_analyzer.app as app
 import pytest
 import os
+import psycopg2
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def setup_module():
+    db = psycopg2.connect(DATABASE_URL)
+    with open("tests/fixtures/test_setup.sql") as f:
+        setup_sql = f.read()
+    with db.cursor() as cursor:
+        cursor.execute(setup_sql)
+        db.commit()
+
+
+def teardown_module():
+    db = psycopg2.connect(DATABASE_URL)
+    with open("tests/fixtures/test_teardown.sql") as f:
+        teardown_sql = f.read()
+    with db.cursor() as cursor:
+        cursor.execute(teardown_sql)
+        db.commit()
 
 
 def test_initial():
