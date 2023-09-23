f9b7f5fe911fa0ce2f2e09ee31b01ed7e4f83955
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-09-23 18:13:33 +0200

    Revert "Move db creation to the tests"
    This reverts commit 3831127aea4115ec5517d9b9395abda3c035bc0f.

--- tests/test_url.py
+++ tests/test_url.py
@@ -1,32 +1,6 @@
 import pytest
 from page_analyzer.url import URL
 import validators
-import psycopg2
-from pytest_postgresql.janitor import DatabaseJanitor
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-
-@pytest.fixture
-def database(postgresql_proc):
-    janitor = DatabaseJanitor(
-        postgresql_proc.user,
-        postgresql_proc.host,
-        postgresql_proc.port,
-        "my_test_database",
-        postgresql_proc.version,
-        password="secret_password",
-    )
-    janitor.init()
-    db = psycopg2.connect(DATABASE_URL)
-    with open("test.sql") as f:
-        setup_sql = f.read()
-    with db.cursor() as cursor:
-        cursor.execute(setup_sql)
-        db.commit()
-    yield db
-    janitor.drop()
 
 
 @pytest.mark.parametrize(
