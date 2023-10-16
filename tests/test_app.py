13b86b4f200b08eca26f73537a4e52ee2bfc22d8
Ksenia Troshchenkova <ktroshchenkova@ripe.net>
2023-10-16 16:36:55 +0200

    Move common functions to conf test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,44 +1,15 @@
 import page_analyzer.app as app
 import pytest
-import os
-import psycopg2
+from conftest import db_setup, db_teardown
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-LOCAL = os.getenv("LOCAL_TESTS", default=False)
-
-
-def set_path_to_fixture(LOCAL):
-    if LOCAL:
-        path_to_fixture = "tests/fixtures/"
-    else:
-        path_to_fixture = "code/tests/fixtures/"
-    return path_to_fixture
-
-
-def setup_module():
-    db = psycopg2.connect(DATABASE_URL)
-    if LOCAL:
-        with open(f"{set_path_to_fixture(LOCAL)}test_setup_create.sql") as f:
-            setup_sql = f.read()
-        with open(f"{set_path_to_fixture(LOCAL)}test_setup_insert.sql") as f:
-            setup_sql = setup_sql + f.read()
-    else:
-        with open(f"{set_path_to_fixture(LOCAL)}test_setup_insert.sql") as f:
-            setup_sql = f.read()
-    with db.cursor() as cursor:
-        cursor.execute(setup_sql)
-        db.commit()
-
-
-def teardown_module():
-    if LOCAL:
-        db = psycopg2.connect(DATABASE_URL)
-        with open(f"{set_path_to_fixture(LOCAL)}test_teardown.sql") as f:
-            teardown_sql = f.read()
-        with db.cursor() as cursor:
-            cursor.execute(teardown_sql)
-            db.commit()
+@pytest.fixture()
+def db_resource():
+    print("setup")
+    db_setup()
+    yield "db_resource"
+    print("teardown")
+    db_teardown()
 
 
 def test_initial():
